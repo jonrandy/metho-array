6c0f995dfbab96657997da03cddf733ee0ff38c8
Jon Randy <jonrandy@gmail.com>
2022-02-10 15:45:31 +0700

    Adds 'head', 'tail', and 'reverse'

--- metho-array.js
+++ metho-array.js
@@ -7,6 +7,56 @@ const STRING_FLAG = "string"
 
 target[Metho.data] = ARRAY_FLAG
 
+// chunk an array or string in to pieces of given size
+const chunkFunc = function chunk(size) {
+	let res = []
+
+	if (this[Metho.data] == ARRAY_FLAG) {
+		// chunk array
+		let chunk
+		for (let i = 0; i < this.length; i += size) {
+			chunk = this.slice(i, i + size)
+			res.push(chunk)
+		}
+	} else if (this[Metho.data] == STRING_FLAG) {
+		// chunk string
+		res = this.match(new RegExp(".{1," + size + "}", "g"))
+	}
+	return res
+}
+export const chunk = addWithMaybeRegisteredSymbolName(
+	target,
+	chunkFunc,
+	"arrayOrStringChunk"
+)
+
+// reverse - reverse the array (duplicate native method)
+export const reverse = addWithMaybeRegisteredSymbolName(
+	target,
+	function reverse() {
+		return this.reverse()
+	},
+	"arrayOrStringReverse"
+)
+
+// head - return first element of array
+export const head = addWithMaybeRegisteredSymbolName(
+	target,
+	function head() {
+		return this[0]
+	},
+	"arrayOrStringHead"
+)
+
+// tail - return remaining elements of array after head
+export const tail = addWithMaybeRegisteredSymbolName(
+	target,
+	function tail() {
+		return this.slice(1)
+	},
+	"arrayOrStringTail"
+)
+
 function addWithMaybeRegisteredSymbolName(target, func, symbolName) {
 	const registered = Metho.registered(symbolName)
 	let ret
@@ -25,28 +75,3 @@ function addWithMaybeRegisteredSymbolName(target, func, symbolName) {
 	}
 	return ret
 }
-
-
-// chunk an array or string in to pieces of given size
-const chunkFunc = function chunk(size) {
-	let res = []
-
-	if (this[Metho.data] == ARRAY_FLAG) {
-
-		// chunk array
-		let chunk
-		for (let i = 0; i < this.length; i += size) {
-			chunk = this.slice(i, i + size)
-			res.push(chunk)
-		}
-
-	} else if (this[Metho.data] == STRING_FLAG) {
-
-		// chunk string
-		res = this.match(new RegExp('.{1,' + size + '}', 'g'))
-
-	}
-	return res
-}
-
-export const chunk = addWithMaybeRegisteredSymbolName(target, chunkFunc, 'arrayOrStringChunk')
